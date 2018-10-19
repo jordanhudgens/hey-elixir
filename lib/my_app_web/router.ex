a56e0928b17cba1f980bd80a97a4abeaeae5f43d
jordanhudgens <jordan@devcamp.com>
2018-10-19 10:47:24 -0500

    Initial commit

---
+++ lib/my_app_web/router.ex
@@ -0,0 +1,14 @@
+defmodule MyAppWeb.Router do
+  use MyAppWeb, :router
+
+  pipeline :api do
+    plug :accepts, ["json"]
+    plug(:fetch_session)
+  end
+
+  scope "/api", MyAppWeb do
+    pipe_through :api
+    resources "/users", UserController, except: [:new, :edit]
+    post("/users/sign_in", UserController, :sign_in)
+  end
+end
